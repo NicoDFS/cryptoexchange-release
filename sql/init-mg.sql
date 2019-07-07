4a72c1456adba2be1889663e48bb1576fe8e0642
Michael Liao <askxuefeng@gmail.com>
2019-07-07 12:36:27 +0800

    add sql/

---
+++ sql/init-mg.sql
@@ -0,0 +1,2 @@
+USE mg;
+INSERT INTO managers (id, secretKey, name, email, random, passwd, permissions, createdAt, updatedAt, version) VALUES (1, '', 'root', 'root@example.com', 'nkNJfhaMMluC945l3NoA', 'e893a5eb710959db8f7279c122495ccfafcc8677cc4f5380b54652d19608fac7', '*', 1562474058091, 1562474058091, 0);
